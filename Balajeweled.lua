08813d68d37f5d04848619ad05a0a00387396d8d
ARandomTank7 <theodubout62@outlook.fr>
2025-01-24 09:19:14 +0100

    Modify atlas_path, ranks and description

--- Balajeweled.lua
+++ Balajeweled.lua
@@ -10,13 +10,13 @@
 
 local atlas_key = 'BJW' -- Format: PREFIX_KEY
 -- See end of file for notes
-local atlas_path = 'imagename.png' -- Filename for the image in the asset folder
+local atlas_path = 'balajeweled.png' -- Filename for the image in the asset folder
 local atlas_path_hc = nil -- Filename for the high-contrast version of the texture, if existing
 
 local suits = {'hearts', 'clubs', 'diamonds', 'spades'} -- Which suits to replace
-local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",} -- Which ranks to replace
+local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', "Ace",} -- Which ranks to replace
 
-local description = 'In-game name of texture in selection menu' -- English-language description, also used as default
+local description = 'Balajeweled' -- English-language description, also used as default
 
 -----------------------------------------------------------
 -- You should only need to change things above this line --
