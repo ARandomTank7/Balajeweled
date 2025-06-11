9771f3618c2f4c2ed31a46a9c3f0c363ab0fddab
ARandomTank7 <theodubout62@protonmail.com>
2025-06-11 13:47:39 +0200

    Add Malverk dependency for enhancers, config screen and update README

--- Balajeweled.lua
+++ Balajeweled.lua
@@ -1,14 +1,45 @@
+local config = SMODS.current_mod.config
 local atlas_key = 'BJW'
 
 local atlas_path = 'balajeweled.png'
 local atlas_path_hc = nil
 
-local enhancers_path = 'bjw_enhancers.png'
-
 local suits = {'hearts', 'spades', 'clubs', 'diamonds'}
 local ranks = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"}
 
 local description = 'Balajeweled'
+
+-- Config screen part
+SMODS.current_mod.config_tab = function()
+	return {n = G.UIT.ROOT, config = {align = "cm", padding = 0.05, colour = G.C.CLEAR}, nodes = {
+		{n = G.UIT.C, config = {align = "cm", padding = 0.1}, nodes = {
+				create_toggle({id = "disable_custom_sounds",
+							   label = "Disable custom sounds (Requires restart)",
+							   ref_table = config,
+							   ref_value = "disable_custom_sounds"
+							 }),
+		}
+	}}
+}
+end
+
+-- Set up Malverk texture pack for enhancers
+AltTexture({
+    key = 'BJW_enhancers',
+    set = 'Enhanced',
+    path = 'bjw_enhancers.png',
+    keys = {'m_mult', 'm_bonus', "m_wild"},
+    original_sheet = 'true'
+})
+
+TexturePack({
+    key = 'balajeweled',
+    textures = {'BJW_enhancers'},
+    loc_txt = {
+        name = 'Balajeweled Enhancers',
+        text = {'Enhancers texture pack for Balajeweled'}
+    }
+})
 
 -- Deck skin part
 
@@ -26,14 +57,6 @@ SMODS.Atlas{
     path = atlas_path,
     prefix_config = {key = false}, 
 }
-
-SMODS.Atlas{
-	key = "centers",
-	path = enhancers_path,
-	px = 71,
-	py = 95,
-	prefix_config = { key = false }
-}:register()
 
 if atlas_path_hc then
     SMODS.Atlas{  
@@ -61,37 +84,39 @@ end
 
 -- Sounds part
 
--- +Mult Sound
-SMODS.Sound({
-    key = "bjw_twist_multup",
-    path = "bjw_twist_multup.ogg",
-    replace = 'multhit1'
-})
+if not config.disable_custom_sounds then
+    -- +Mult Sound
+    SMODS.Sound({
+        key = "bjw_twist_multup",
+        path = "bjw_twist_multup.ogg",
+        replace = 'multhit1'
+    })
 
--- Scoring sound
-SMODS.Sound({
-    key = "bjw3_match",
-    path = "bjw3_match.ogg",
-    replace = 'chips1'
-})
+    -- Scoring sound
+    SMODS.Sound({
+        key = "bjw3_match",
+        path = "bjw3_match.ogg",
+        replace = 'chips1'
+    })
 
--- Polychrome sound
-SMODS.Sound({
-    key = "bjw2_hypercube",
-    path = "bjw2_hypercube.ogg",
-    replace = 'polychrome1'
-})
+    -- Polychrome sound
+    SMODS.Sound({
+        key = "bjw2_hypercube",
+        path = "bjw2_hypercube.ogg",
+        replace = 'polychrome1'
+    })
 
--- Foil sound
-SMODS.Sound({
-    key = "bjw2_powergem",
-    path = "bjw2_powergem.ogg",
-    replace = 'foil1'
-})
+    -- Foil sound
+    SMODS.Sound({
+        key = "bjw2_powergem",
+        path = "bjw2_powergem.ogg",
+        replace = 'foil1'
+    })
 
--- Foil triggered sound
-SMODS.Sound({
-    key = "bjw2_mystery",
-    path = "bjw2_mystery.ogg",
-    replace = 'foil2'
-})
+    -- Foil triggered sound
+    SMODS.Sound({
+        key = "bjw2_mystery",
+        path = "bjw2_mystery.ogg",
+        replace = 'foil2'
+    }) 
+end
--- README.md
+++ README.md
@@ -6,17 +6,24 @@ Bejeweled-themed deck skin and SFXs for Balatro
 
 ## How to install Balajeweled (the easy way)
 0. Install [Lovely Injector](https://github.com/ethangreen-dev/lovely-injector) and skyline69's [**B**alatro **M**od **M**anager](https://github.com/skyline69/balatro-mod-manager)
-1. After setting up **BMM**, search for Steamodded and install it as Balajeweled requires Steamodded.
+1. After setting up **BMM**, search for **Steamodded** and **Malverk** and install them as Balajeweled requires them.
 2. Look for Balajeweled, either in the "Resource Packs" category or by searching manually then install it.
 3. Upon entering the game, go to "Options" and "Customize deck", set all suits to "Balajeweled"
-4. If everything's fine, you are all set :D
+4. Go back in the options menu and go to "Textures" then enable "Balajeweled Enhancers"
+5. If everything's fine, you are all set :D
 
 ## How to install Balajeweled (the classic way)
-0. Install [Lovely Injector](https://github.com/ethangreen-dev/lovely-injector) and [Steamodded](https://github.com/Steamodded/smods/archive/refs/tags/1.0.0-beta-0323b.zip) **_(Your AV may freak out after downloading Lovely, don't fret, Lovely is completely safe to download, you'll need to whitelist Lovely in your AV settings)_**
-1. Go to the Releases page on the right panel and download the zip file of the latest release or click **[here](https://github.com/ARandomTank7/Balajeweled/releases/latest/download/Balajeweled.zip)** to download the latest version directly.
-2. Extract the "Balajeweled" folder from the zip file inside your Balatro mods folder (usually at C:/Users/[yourname]/AppData/Roaming/Balatro/Mods (Replace "[yourname]" by your Windows username))
+
+> [!WARNING]
+> Your antivirus may freak out after downloading Lovely, don't panic, Lovely is completely safe as it is open source, you'll need to whitelist Lovely in your AV settings.
+
+0. Install [Lovely Injector](https://github.com/ethangreen-dev/lovely-injector), [Steamodded](https://github.com/Steamodded/smods) and [Malverk](https://github.com/Eremel/Malverk)
+1. Go to the Releases page and download the zip file of the latest release or **[click here](https://github.com/ARandomTank7/Balajeweled/releases/latest/download/Balajeweled.zip)** to download the latest version directly.
+2. Extract the contents of the zip file in a folder called "Balajeweled" inside your Balatro mods folder (usually at C:/Users/[yourname]/AppData/Roaming/Balatro/Mods (Replace "[yourname]" by your Windows username))
 3. Upon entering the game, go to "Options" and "Customize deck", set all suits to "Balajeweled"
-4. If everything's fine, you are all set :D
+4. Go back in the options menu and go to "Textures" then enable "Balajeweled Enhancers"
+5. If everything's fine, you are all set :D
+
 
 ## Questions and Answers
 *Q: What has Bejeweled have to do with poker?*  
