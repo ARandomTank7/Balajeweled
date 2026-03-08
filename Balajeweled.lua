db7b8d586edb4bd40b00bc8fa9a4d51b49bd5b64
ARandomTank7 <theodubout62@outlook.fr>
2026-03-08 10:15:03 +0100

    Prepare refactor

--- Balajeweled.lua
+++ Balajeweled.lua
@@ -1,13 +1,5 @@
 local config = SMODS.current_mod.config
-local atlas_key = 'BJW'
-
-local atlas_path = 'balajeweled.png'
-local atlas_path_hc = nil
-
-local suits = {'hearts', 'spades', 'clubs', 'diamonds'}
 local ranks = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"}
-
-local description = 'Balajeweled'
 
 -- Config screen part
 SMODS.current_mod.config_tab = function()
@@ -43,44 +35,6 @@ TexturePack({
 
 -- Deck skin part
 
-SMODS.Atlas({
-    key = 'modicon',
-    path = 'modicon.png',
-    px = '34',
-    py = '34'
-})
-
-SMODS.Atlas{  
-    key = atlas_key..'_lc',
-    px = 71,
-    py = 95,
-    path = atlas_path,
-    prefix_config = {key = false}, 
-}
-
-if atlas_path_hc then
-    SMODS.Atlas{  
-        key = atlas_key..'_hc',
-        px = 71,
-        py = 95,
-        path = atlas_path_hc,
-        prefix_config = {key = false}, 
-    }
-end
-
-for _, suit in ipairs(suits) do
-    SMODS.DeckSkin{
-        key = suit.."_skin",
-        suit = suit:gsub("^%l", string.upper),
-        ranks = ranks,
-        lc_atlas = atlas_key..'_lc',
-        hc_atlas = (atlas_path_hc and atlas_key..'_hc') or atlas_key..'_lc',
-        loc_txt = {
-            ['en-us'] = description
-        },
-        posStyle = 'deck'
-    }
-end
 
 -- Sounds part
 
